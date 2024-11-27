b1d9b55f94963fe1fa872eb48f87b157dbd1001e
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-27 11:14:16 -0600

    Creating initial configurations, installing tailwind css and shadcn with components

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '../components/ui/button';
+import { Label } from '../components/ui/label';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+     <Button>shadcn button</Button>
+     <Label>this is a label shadcn</Label>
+    </>
+  )
+}
+
+export default App;
