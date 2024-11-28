a5ccc8de924376865154be67be9d217aae303fb7
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-28 11:57:34 -0600

    creating the desing of the error page

--- src/features/error/Error.tsx
+++ src/features/error/Error.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage: React.FC = () => {
   return (
-    <div className="div">error Page</div>
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-4">
+      <Label className="text-[2rem] tracking-wide font-bold">404 Not Found</Label>
+      <Label className="text-[1.2rem] tracking-wide font-semibold">Esta pagina no existe</Label>
+      <Link to="/home">
+        <Button className="bg-blue text-surface-neutral h-[3rem] tracking-wide font-semibold">Regresar</Button>
+      </Link>
+    </div>
   );
 }
 
