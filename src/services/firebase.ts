67bd3820000d228f5753316977a43b993021fdfd
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-28 13:17:01 -0600

    credentials of firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { FirebaseAppProvider } from "reactfire";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDvWYrDKD-JDjBo1hURl-G7vdhuy1P6Pmc",
+  projectId: "fitem-store-4a2ea",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db, FirebaseAppProvider };
