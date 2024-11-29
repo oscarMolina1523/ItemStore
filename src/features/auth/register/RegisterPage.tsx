cd3b202f5c98d715d203731de06b3807e9790419
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-28 20:20:51 -0600

    creating auth pages

--- src/features/auth/register/RegisterPage.tsx
+++ src/features/auth/register/RegisterPage.tsx
@@ -1,8 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ArrowLeft } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterPage: React.FC = () => {
   return (
-    <div className="div">this is the register page</div>
+    <div className="flex flex-col items-center gap-4 px-4 h-screen">
+      <div className="flex flex-col items-center w-full gap-4 ">
+        <Link to="/splash" className="flex flex-row gap-4 items-center w-full fixed top-0 left-0 right-0 z-20 h-[4rem] bg-surface-neutral mb-6">
+          <ArrowLeft className="h-8 w-8" />
+          <Label className="text-[1.5rem] text-black font-semibold tracking-wide">Registrarse</Label>
+        </Link>
+      </div>
+      <div className="flex flex-col gap-4 items-center w-full mt-[4rem]">
+        <div className="flex flex-col gap-1 text-left w-full">
+          <Label className="text-[1.2rem] tracking-wide text-black">Nombre</Label>
+          <Input className="h-[3rem]" placeholder="ej. Juan Jose Sanchez Rodriguez"/>
+        </div>
+        <div className="flex flex-col gap-1 text-left w-full">
+          <Label className="text-[1.2rem] tracking-wide text-black">E-mail</Label>
+          <Input className="h-[3rem]" placeholder="ej. jorge@gmail.com"/>
+        </div>
+        <div className="flex flex-col gap-1 text-left w-full">
+          <Label className="text-[1.2rem] tracking-wide text-black">Contraseña</Label>
+          <Input type="password" className="h-[3rem]" placeholder="ej. ghHJ5V!h"/>
+        </div>
+        <div className="flex flex-col gap-1 w-full">
+          <Button className="bg-blue h-[3rem] text-[1.2rem] tracking-wide font-semibold text-surface-neutral hover:bg-surface-neutral hover:text-blue hover:border hover:border-blue">Crear Cuenta</Button>
+        </div>
+        <div className="flex flex-row gap-1 w-full items-center">
+          <div className="w-1/2 h-[1px] bg-black"></div>
+          <Label className=" tracking-wide text-black">O</Label>
+          <div className="w-1/2 h-[1px] bg-black"></div>
+        </div>
+        <div className="flex flex-row gap-1 items-center justify-center w-full">
+          <Label className=" tracking-wide text-black">Ya posee una cuenta?</Label>
+          <Link to="/login" className="font-semibold tracking-wide text-blue">Iniciar Sesion</Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/features/auth/splash/SplashPage.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const SplashPage: React.FC = () => {
+  return (
+    <div className="w-full flex flex-col h-screen bg-blue item-center">
+      <div className="h-2/4 flex items-end justify-center">
+        <img className="object-contain "
+        src="src\assets\splash.svg" 
+        alt="splash-img" 
+        />
+      </div>
+      <div className="flex h-2/4 flex-col gap-2 items-center justify-center w-full px-4">
+        <Button className="h-[4rem] w-full hover:text-surface-neutral hover:bg-blue hover:border-surface-neutral tracking-wide text-[1.2rem] font-semibold text-blue bg-surface-neutral border border-blue">
+          Iniciar Sesion
+        </Button>
+        <Button  className="h-[4rem] w-full hover:text-blue hover:bg-surface-neutral hover:border-blue  tracking-wide text-[1.2rem] font-semibold bg-blue text-surface-neutral">
+          Registrarse
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SplashPage;
