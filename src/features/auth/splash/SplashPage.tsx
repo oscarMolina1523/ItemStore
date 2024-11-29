8c276d2e45eb625250c70856788478a85e3a985f
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-28 20:22:26 -0600

    other changes in this pages

--- src/features/auth/splash/SplashPage.tsx
+++ src/features/auth/splash/SplashPage.tsx
@@ -6,17 +6,21 @@ const SplashPage: React.FC = () => {
     <div className="w-full flex flex-col h-screen bg-blue item-center">
       <div className="h-2/4 flex items-end justify-center">
         <img className="object-contain "
-        src="src\assets\splash.svg" 
-        alt="splash-img" 
+          src="src\assets\splash.svg"
+          alt="splash-img"
         />
       </div>
       <div className="flex h-2/4 flex-col gap-2 items-center justify-center w-full px-4">
-        <Button className="h-[4rem] w-full hover:text-surface-neutral hover:bg-blue hover:border-surface-neutral tracking-wide text-[1.2rem] font-semibold text-blue bg-surface-neutral border border-blue">
-          Iniciar Sesion
-        </Button>
-        <Button  className="h-[4rem] w-full hover:text-blue hover:bg-surface-neutral hover:border-blue  tracking-wide text-[1.2rem] font-semibold bg-blue text-surface-neutral">
-          Registrarse
-        </Button>
+        <div className="w-full">
+          <Button className="h-[4rem] w-full hover:text-surface-neutral hover:bg-blue hover:border-surface-neutral tracking-wide text-[1.2rem] font-semibold text-blue bg-surface-neutral border border-blue">
+            Iniciar Sesion
+          </Button>
+        </div>
+        <div className="w-full">
+          <Button className="h-[4rem] w-full hover:text-blue hover:bg-surface-neutral hover:border-blue  tracking-wide text-[1.2rem] font-semibold bg-blue text-surface-neutral">
+            Registrarse
+          </Button>
+        </div>
       </div>
     </div>
   );
