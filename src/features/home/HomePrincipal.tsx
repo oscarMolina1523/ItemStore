3c25b2db13197d1ed7057909cf1ffadb305b2e99
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-30 10:18:49 -0600

    obtaining the list of product and implementing the logic to show the data of the products created

--- src/features/home/HomePrincipal.tsx
+++ src/features/home/HomePrincipal.tsx
@@ -7,29 +7,46 @@ import {
   CarouselPrevious
 } from "@/components/ui/carousel";
 import { Label } from "@/components/ui/label";
+import { Producto } from "@/models/EntitiesModel";
+import { ProductService } from "@/services/ProductService";
 import { Search } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const HomePrincipalComponent: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const items = [
-    {
-      image: "https://images-cdn.ubuy.ae/6567e25a06139139127520a7-bxyjdj-men-39-s-running-shoes-walking.jpg",
-      label: "Popular",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://olympicsa.co.za/wp-content/uploads/2024/02/01.Olympic-Bounce-Men-Blue-Feature-image-Side-A-1080px-x-1080px-jpg-600x600.webp",
-      label: "Popular",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://xeroshoes.com/wp-content/uploads/2024/06/DLLM-WHTE_Dillon-Leather_White_AngleR_0056_WEB-385x250.jpg",
-      label: "Popular",
-      title: "Air Max 2090",
-    },
-  ];
+  const [items, setItems]=useState<Producto[]>([]);
+
+  useEffect(()=>{
+    const fetchProducts = async () => {
+      try {
+        const prod = await ProductService.getProducts();
+        setItems(prod);
+      } catch (error) {
+        console.log("Failed to obtain products", error);
+      }
+    };
+  
+    fetchProducts();
+  },[setItems]);
+
+  // const items = [
+  //   {
+  //     image: "https://images-cdn.ubuy.ae/6567e25a06139139127520a7-bxyjdj-men-39-s-running-shoes-walking.jpg",
+  //     label: "Popular",
+  //     title: "Air Max 2090",
+  //   },
+  //   {
+  //     image: "https://olympicsa.co.za/wp-content/uploads/2024/02/01.Olympic-Bounce-Men-Blue-Feature-image-Side-A-1080px-x-1080px-jpg-600x600.webp",
+  //     label: "Popular",
+  //     title: "Air Max 2090",
+  //   },
+  //   {
+  //     image: "https://xeroshoes.com/wp-content/uploads/2024/06/DLLM-WHTE_Dillon-Leather_White_AngleR_0056_WEB-385x250.jpg",
+  //     label: "Popular",
+  //     title: "Air Max 2090",
+  //   },
+  // ];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -40,7 +57,7 @@ const HomePrincipalComponent: React.FC = () => {
   }, [items.length]);
 
   return (
-    <div className="flex flex-col items-center h-[15rem] w-full relative">
+    <div className="flex flex-col items-center h-[20rem] w-full relative">
       <div className="w-full h-4/5 bg-blue rounded-b-[3rem]"></div>
       <div className="flex flex-col absolute items-center justify-center w-5/7 gap-4 px-3 flex-grow translate-y-1/7 bottom-6">
         <Link to="/search" className="w-full">
@@ -62,18 +79,17 @@ const HomePrincipalComponent: React.FC = () => {
                   <div className="w-3/5">
                     <img
                       className="object-cover h-[8rem] w-full rounded-md border border-dark-gray"
-                      src={item.image}
+                      src={item.imagen}
                       alt={`product-${index}`}
                     />
                   </div>
                   <div className="w-2/5 flex flex-col gap-1 items-start text-left pl-1">
                     <Label className="text-black font-semibold">
-                      {item.label}
+                      C${item.precio}
                     </Label>
                     <Label className="text-black font-extrabold text-[1.2rem]">
-                      {item.title}
+                      {item.titulo}
                     </Label>
-                    <Button>Buy Now</Button>
                   </div>
                 </CarouselItem>
               ))}
