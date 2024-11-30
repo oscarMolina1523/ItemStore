ae3042c385165356b732f97b5ef5e0c206d15932
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-30 11:52:21 -0600

    adding the get logic to obtain the data

--- src/features/home/HomePrincipal.tsx
+++ src/features/home/HomePrincipal.tsx
@@ -15,9 +15,9 @@ import { Link } from "react-router-dom";
 
 const HomePrincipalComponent: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [items, setItems]=useState<Producto[]>([]);
+  const [items, setItems] = useState<Producto[]>([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchProducts = async () => {
       try {
         const prod = await ProductService.getProducts();
@@ -26,9 +26,9 @@ const HomePrincipalComponent: React.FC = () => {
         console.log("Failed to obtain products", error);
       }
     };
-  
+
     fetchProducts();
-  },[setItems]);
+  }, [setItems]);
 
   // const items = [
   //   {
@@ -59,22 +59,20 @@ const HomePrincipalComponent: React.FC = () => {
   return (
     <div className="flex flex-col items-center h-[20rem] w-full relative">
       <div className="w-full h-4/5 bg-blue rounded-b-[3rem]"></div>
-      <div className="flex flex-col absolute items-center justify-center w-5/7 gap-4 px-3 flex-grow translate-y-1/7 bottom-6">
+      <div className="flex flex-col absolute items-center justify-center w-full gap-4 px-3 flex-grow translate-y-1/7 bottom-6">
         <Link to="/search" className="w-full">
           <Button className="w-full h-[3rem] flex flex-row justify-start items-center bg-gray text-dark-gray">
             <Search />
             Search...
           </Button>
         </Link>
-        <div>
+        <div className="w-full">
           <Carousel className="bg-surface-neutral rounded-lg py-2 px-2 flex flex-col items-center shadow-lg">
-            <CarouselContent>
+            <CarouselContent className="w-full">
               {items.map((item, index) => (
                 <CarouselItem
                   key={index}
-                  className={`flex flex-row w-full ${
-                    currentIndex === index ? "hidden" : "flex"
-                  }`}
+                  className={`flex flex-row w-full ${currentIndex === index ? "flex" : "hidden"}`}
                 >
                   <div className="w-3/5">
                     <img
@@ -83,11 +81,11 @@ const HomePrincipalComponent: React.FC = () => {
                       alt={`product-${index}`}
                     />
                   </div>
-                  <div className="w-2/5 flex flex-col gap-1 items-start text-left pl-1">
+                  <div className="w-2/5 flex flex-col gap-1 items-start text-left pl-1 p-0">
                     <Label className="text-black font-semibold">
                       C${item.precio}
                     </Label>
-                    <Label className="text-black font-extrabold text-[1.2rem]">
+                    <Label className="text-black font-extrabold text-[1.2rem] flex-wrap ">
                       {item.titulo}
                     </Label>
                   </div>
@@ -97,9 +95,7 @@ const HomePrincipalComponent: React.FC = () => {
             <div className="hidden justify-center gap-2 absolute bottom-0 translate-y-3/4 py-4">
               <CarouselPrevious
                 onClick={() =>
-                  setCurrentIndex(
-                    (prevIndex) => (prevIndex - 1 + items.length) % items.length
-                  )
+                  setCurrentIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length)
                 }
                 className="border border-transparent bg-transparent"
               />
