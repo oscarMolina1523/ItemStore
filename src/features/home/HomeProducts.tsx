120c28b6916d9a6d0ee248ab1162210467908287
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-30 12:14:47 -0600

    adding sort in products of home

--- src/features/home/HomeProducts.tsx
+++ src/features/home/HomeProducts.tsx
@@ -1,57 +1,38 @@
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import { Label } from "@/components/ui/label";
+import { Producto } from "@/models/EntitiesModel";
+import { ProductService } from "@/services/ProductService";
 import ProductDetailComponent from "@/shared/ProductDetailComponent";
 import { Heart } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const HomeProductsComponent: React.FC = () => {
   const [selectedProd, setSelectedProd] = useState(false);
+  const [items, setItems] = useState<Producto[]>([]);
 
   const handleSelectedProduct = () => {
     setSelectedProd(!selectedProd);
   }
 
-  const items = [
-    {
-      image: "https://images-cdn.ubuy.ae/6567e25a06139139127520a7-bxyjdj-men-39-s-running-shoes-walking.jpg",
-      color: "red",
-      price: "$50",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://olympicsa.co.za/wp-content/uploads/2024/02/01.Olympic-Bounce-Men-Blue-Feature-image-Side-A-1080px-x-1080px-jpg-600x600.webp",
-      color: "blue",
-      price: "$70",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://xeroshoes.com/wp-content/uploads/2024/06/DLLM-WHTE_Dillon-Leather_White_AngleR_0056_WEB-385x250.jpg",
-      color: "white",
-      price: "$100",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://xeroshoes.com/wp-content/uploads/2024/06/DLLM-WHTE_Dillon-Leather_White_AngleR_0056_WEB-385x250.jpg",
-      color: "white",
-      price: "$100",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://xeroshoes.com/wp-content/uploads/2024/06/DLLM-WHTE_Dillon-Leather_White_AngleR_0056_WEB-385x250.jpg",
-      color: "white",
-      price: "$100",
-      title: "Air Max 2090",
-    },
-    {
-      image: "https://xeroshoes.com/wp-content/uploads/2024/06/DLLM-WHTE_Dillon-Leather_White_AngleR_0056_WEB-385x250.jpg",
-      color: "white",
-      price: "$100",
-      title: "Air Max 2090",
-    },
-  ];
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const prod = await ProductService.getProducts();
+        setItems(prod);
+      } catch (error) {
+        console.log("Failed to obtain products", error);
+      }
+    };
+
+    fetchProducts();
+  }, [setItems]);
+
+
+  const sortedItems = [...items].sort((a, b) => {
+    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+  });
 
   return (
     <div className="flex flex-col items-center bg-dark-gray rounded-t-[1rem] w-full flex-grow mt-8 gap-6">
@@ -62,24 +43,32 @@ const HomeProductsComponent: React.FC = () => {
       <div className="px-2">
         <Carousel className="w-full max-w-sm">
           <CarouselContent onClick={handleSelectedProduct} className="ml-2 mb-4">
-            {items.map((item, index) => (
+            {sortedItems.slice(0).map((item, index) => (
               <CarouselItem key={index} className="pl-1 basis-[11rem]">
                 <Card>
-                  <CardContent className="flex flex-col aspect-square p-1 flex-grow">
+                  <CardContent className="flex flex-col flex-grow aspect-square p-1 flex-grow">
                     <div className="w-full h-3/4 relative">
                       <img
                         className="object-cover h-full w-full rounded-md border border-dark-gray relative"
-                        src={item.image}
+                        src={item.imagen}
                         alt={`product-${index}`}
                       />
                       {/* <Heart className="h-4 w-4 top-2 right-2 absolute text-red"/> */}
                     </div>
-                    <span className="text-[1.2rem] font-semibold tracking-wide text-left">{item.title}</span>
-                    <div className="inline-flex">
-                      <Badge className="inline-flex px-2 bg-dark-gray text-surface-neutral">{item.color}</Badge>
-                    </div>
+                    <span className="font-semibold tracking-wide text-left">{item.titulo}</span>
+                    {/* <div className="flex flex-wrap gap-2">
+                      {item.categorias.map((categoria, index) => (
+                        <Badge
+                          key={index}
+                          className="px-2 bg-dark-gray text-surface-neutral"
+                        >
+                          {categoria}
+                        </Badge>
+                      ))}
+                    </div> */}
+                    <br/>
                     <div className="flex flex-row items-center justify-between">
-                      <span className="text-[1.2rem] font-extrabold tracking-wide text-left">{item.price}</span>
+                      <span className="text-[1.2rem] font-extrabold tracking-wide text-left">C${item.precio}</span>
                       <Heart className="h-6 w-6" />
                     </div>
                   </CardContent>
@@ -89,7 +78,7 @@ const HomeProductsComponent: React.FC = () => {
           </CarouselContent>
         </Carousel>
       </div>
-      <ProductDetailComponent show={selectedProd} onClose={handleSelectedProduct}/>
+      <ProductDetailComponent show={selectedProd} onClose={handleSelectedProduct} />
     </div>
   );
 }
