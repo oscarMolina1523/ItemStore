23b00d8f932c44bf432d62924e0195554176421a
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-11-30 12:37:31 -0600

    showing the data of the selected product in home

--- src/shared/ProductDetailComponent.tsx
+++ src/shared/ProductDetailComponent.tsx
@@ -1,23 +1,27 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { Producto } from "@/models/EntitiesModel";
 import { Heart, X } from "lucide-react";
 import React from "react";
 
 interface ProductDetailComponentProps {
   show: boolean;
   onClose: () => void;
+  product: Producto;
 }
 
 const ProductDetailComponent: React.FC<ProductDetailComponentProps> = ({
   show,
-  onClose
+  onClose,
+  product
 }) => {
   if (!show) return null;
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center overflow-y-auto bg-black bg-opacity-40 z-50 transition-opacity duration-500 ease-[cubic-bezier(0.42, 0, 0.58, 1)]">
-      <Card className="w-full md:w-1/2 mt-20 border ">
+      <Card className="w-full md:w-1/2 border mt-20">
         <CardDescription>
           <div className="flex flex-row items-center justify-between px-4 py-4 border-b-2 border-surface-neutral text-black">
             <Label className="text-[1.2rem]">Detalle Producto</Label>
@@ -27,15 +31,26 @@ const ProductDetailComponent: React.FC<ProductDetailComponentProps> = ({
         <CardContent>
           <div className="flex w-full h-1/2">
             <img className="object-cover w-full h-full"
-              src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_1_1200x1200/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=1-9sfjwH"
+              src={product.imagen}
               alt="profile-img" />
           </div>
-          <div className="flex flex-col gap-2 items-start">
-            <Label>Nombre:</Label>
-            <Label>Descripcion:</Label>
-            <Label>Categoria:</Label>
-            <Label>Precio:</Label>
+          <div className="flex flex-col gap-2 items-start text-left">
+            <p><strong className="font-semibold tracking-wide">Nombre: </strong>{product.titulo}</p>
+            <p><strong className="font-semibold tracking-wide">Descripcion: </strong>{product.descripcion}</p>
+            <p><strong className="font-semibold tracking-wide">Precio: C$</strong>{product.precio}</p>
           </div>
+          <br />
+          <div className="flex flex-wrap gap-2">
+            {product.categorias.map((categoria, index) => (
+              <Badge
+                key={index}
+                className="px-2 bg-blue text-surface-neutral"
+              >
+                {categoria}
+              </Badge>
+            ))}
+          </div>
+          <br />
           <div>
             <Button><Heart />Agregar a lista de deseos</Button>
           </div>
