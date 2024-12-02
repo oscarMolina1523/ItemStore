9e12c63ad2e2cac91ab7974726dfae2dcaa4a188
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-12-01 19:02:08 -0600

    creating a product component reusable

---
+++ src/shared/ProductComponent.tsx
@@ -0,0 +1,42 @@
+import { Badge } from "@/components/ui/badge";
+import { Label } from "@/components/ui/label";
+import React from "react";
+
+interface ProductComponentProps {
+  imageUrl: string;
+  category: string[];
+  title: string;
+  onClick: () => void;
+}
+
+const ProductComponent: React.FC<ProductComponentProps> = ({
+  imageUrl,
+  category,
+  title,
+  onClick
+}) => {
+  return (
+    <div onClick={onClick} className="flex flex-col gap-2 w-full h-full shadow-2xl pb-2">
+      <div className="w-full h-[12.5rem]">
+        <img
+          className="object-cover w-full h-full"
+          src={imageUrl}
+          alt="post-image"
+        />
+      </div>
+      <div className="inline-flex gap-2 flex-wrap">
+        {category.map((categoria, index) => (
+          <Badge
+            key={index}
+            className="px-2 bg-blue text-surface-neutral"
+          >
+            {categoria}
+          </Badge>
+        ))}
+      </div>
+      <Label className="text-ocean-blue dark:text-surface-neutral tracking-wide text-left font-semibold">{title}</Label>
+    </div>
+  );
+}
+
+export default ProductComponent;
