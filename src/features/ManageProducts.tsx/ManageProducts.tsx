a7bd421a8e2ed118ad017a54f74f384f4b413b5e
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-12-03 18:32:51 -0600

    implementing edit and delete functions in manage products

--- src/features/ManageProducts.tsx/ManageProducts.tsx
+++ src/features/ManageProducts.tsx/ManageProducts.tsx
@@ -1,32 +1,35 @@
 import CreateProductComponent from "@/components/itemStoreComponents/CreateProduct";
+import EditProductComponent from "@/components/itemStoreComponents/EditProduct";
 import { Button } from "@/components/ui/button";
 import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
 } from "@/components/ui/command";
 import { Label } from "@/components/ui/label";
 import { useProductContext } from "@/context/ProductContext";
 import { useSingleProductContext } from "@/context/SingleProductContext";
 import { Producto } from "@/models/EntitiesModel";
-import ProductComponent from "@/shared/ProductComponent";
+import { ProductService } from "@/services/ProductService";
 import ProductDetailComponent from "@/shared/ProductDetailComponent";
+import ProductProfileComponent from "@/shared/ProductProfileComponent";
 import { ArrowLeft } from "lucide-react";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const ManageProductsPage: React.FC = () => {
   const { fetchSingleProduct } = useSingleProductContext();
-  const { products, loadingProd, errorProd } = useProductContext();
+  const { products, loadingProd, errorProd, refetchProducts } = useProductContext();
   const [visibleProducts, setVisibleProducts] = useState<number>(12);
-  const [search, setSearch] = useState<string>(""); 
-  const [filteredCategories, setFilteredCategories] = useState<string[]>([]); // Categorías filtradas dinámicamente
-  const [newProduct, setNewProduct]= useState(false);
+  const [search, setSearch] = useState<string>("");
+  const [filteredCategories, setFilteredCategories] = useState<string[]>([]); 
+  const [newProduct, setNewProduct] = useState(false);
+  const [editProd, setEditProd]=useState<Producto| null>(null);
 
-  const handleNewProductClick=()=>{
+  const handleNewProductClick = () => {
     setNewProduct(!newProduct);
   }
 
@@ -68,6 +71,15 @@ const ManageProductsPage: React.FC = () => {
     if (product) fetchSingleProduct(product.id);
   };
 
+  const handleDeleteClick = (postId: string) => {
+    ProductService.deleteProduct(postId);
+    refetchProducts();
+  };
+
+  const handleEditClick = (post: Producto) => {
+    setEditProd(post);
+  };
+
   return (
     <div className="w-full flex flex-col gap-4 bg-surface-neutral dark:bg-dark-ocean-blue items-center px-2 py-6">
       <div className="flex flex-col items-center w-full gap-4 ">
@@ -76,9 +88,9 @@ const ManageProductsPage: React.FC = () => {
           <Label className="text-[1.5rem] text-black font-semibold tracking-wide">Administrador de productos</Label>
         </Link>
       </div>
-      <CreateProductComponent show={newProduct} onClose={handleNewProductClick}/>
+      <CreateProductComponent show={newProduct} onClose={handleNewProductClick} />
       <div className="mt-[3rem]">
-        <Button  onClick={handleNewProductClick} className="bg-blue font-semibold tracking-wide text-surface-neutral">Nuevo producto</Button>
+        <Button onClick={handleNewProductClick} className="bg-blue font-semibold tracking-wide text-surface-neutral">Nuevo producto</Button>
       </div>
       <Command className="rounded-lg border w-full md:w-3/4 bg-surface-neutral text-dark-ocean-blue dark:bg-dark-ocean-blue dark:text-surface-neutral">
         <CommandInput
@@ -116,12 +128,13 @@ const ManageProductsPage: React.FC = () => {
             <div>error products</div>
           ) : filteredProducts.length > 0 ? (
             filteredProducts.slice(0, visibleProducts).map((product) => (
-              <ProductComponent
-                key={product.id}
+              <ProductProfileComponent
                 category={product.categorias}
                 title={product.titulo}
                 imageUrl={product.imagen}
                 onClick={() => handleSelectedProduct(product)}
+                onDeleteClick={() => handleDeleteClick(product.id)}
+                onEditClick={()=>handleEditClick(product)}
               />
             ))
           ) : (
@@ -143,6 +156,14 @@ const ManageProductsPage: React.FC = () => {
           onClose={() => handleSelectedProduct(undefined)}
         />
       )}
+       {editProd && ( 
+        <EditProductComponent 
+          show={!!editProd} 
+          onClose={() => setEditProd(null)} 
+          productToEdit={editProd} 
+        />
+        )}
+
     </div>
   );
 };
