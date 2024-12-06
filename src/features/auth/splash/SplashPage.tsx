5e861d7121c747b7880866144fbde0109fddfd56
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-12-05 19:37:48 -0600

    adding other option to redirect to home page

--- src/features/auth/splash/SplashPage.tsx
+++ src/features/auth/splash/SplashPage.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { ArrowRight } from "lucide-react";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -21,6 +23,10 @@ const SplashPage: React.FC = () => {
           <Button className="h-[4rem] w-full hover:text-blue hover:bg-surface-neutral hover:border-blue  tracking-wide text-[1.2rem] font-semibold bg-blue text-surface-neutral">
             Registrarse
           </Button>
+        </Link>
+        <Link to="/home" className="flex flex-row gap-2 w-full h-[4rem] items-center justify-center">
+          <Label className="text-surface-neutral font-semibold tracking-wide text-[1.2rem] underline">Saltar estos pasos</Label>
+          <ArrowRight className="text-surface-neutral h-8 w-8"/>
         </Link>
       </div>
     </div>
