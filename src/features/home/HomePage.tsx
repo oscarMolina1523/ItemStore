b0a426a05be64767ec6efaaac5cd06265c1b36d1
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-12-06 19:28:54 -0600

    implementign the logic to show or hidde components

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeCategoriesComponent from "./HomeCategories";
 import HomePrincipalComponent from "./HomePrincipal";
+import HomePrincipalMobileComponent from "./HomePrincipalMobile";
 import HomeProductsComponent from "./HomeProducts";
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex flex-col w-full flex-grow items-center">
+    <div className="flex flex-col w-full h-full items-center">
+      <HomePrincipalMobileComponent/>
       <HomePrincipalComponent/>
       <HomeCategoriesComponent/>
       <HomeProductsComponent/>
